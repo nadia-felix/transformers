83dc5e190b6763ef84df8050177c17c60cc8bd9c
nadia-felix <55856846+nadia-felix@users.noreply.github.com>
2020-04-08 20:45:17 -0300

    Update run_glue.py

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -588,6 +588,7 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(
         args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
         do_lower_case=args.do_lower_case,
+        config=config,
         cache_dir=args.cache_dir if args.cache_dir else None,
     )
     model = AutoModelForSequenceClassification.from_pretrained(
